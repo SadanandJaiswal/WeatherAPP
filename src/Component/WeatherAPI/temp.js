370169baaf96d86d4553f29ed5414adc083bc372
Sadanand Jaiswal <sadanandjaiswal92@gmail.com>
2023-05-27 21:06:22 +0530

    changes due to deployment

--- src/Component/WeatherAPI/temp.js
+++ src/Component/WeatherAPI/temp.js
@@ -6,37 +6,37 @@ const Temp = () => {
   const [searchValue, setSearchValue] = useState("pune");
   const [tempInfo, setTempInfo] = useState({});
 
+const getWeatherInfo = useCallback( async () => {
+  try {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=cc9e96a8fd0ea585e3115b015d1c912b`;
 
+    let res = await fetch(url);
+    let data = await res.json();
 
-  const getWeatherInfo = async () => {
-    try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=cc9e96a8fd0ea585e3115b015d1c912b`;
+    const { temp, humidity, pressure } = data.main;
+    const { main: weathermood } = data.weather[0];
+    const { name } = data;
+    const { speed } = data.wind;
+    const { country, sunset } = data.sys;
 
-      let res = await fetch(url);
-      let data = await res.json();
+    const myNewWeatherInfo = {
+      temp,
+      humidity,
+      pressure,
+      weathermood,
+      name,
+      speed,
+      country,
+      sunset,
+    };
 
-      const { temp, humidity, pressure } = data.main;
-      const { main: weathermood } = data.weather[0];
-      const { name } = data;
-      const { speed } = data.wind;
-      const { country, sunset } = data.sys;
+    setTempInfo(myNewWeatherInfo);
+  } catch (error) {
+    console.log(error);
+  }
 
-      const myNewWeatherInfo = {
-        temp,
-        humidity,
-        pressure,
-        weathermood,
-        name,
-        speed,
-        country,
-        sunset,
-      };
+}, []);
 
-      setTempInfo(myNewWeatherInfo);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   useEffect(() => {
     getWeatherInfo();
